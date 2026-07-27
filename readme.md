6612607355d922d9f9d5dca578b6d96a4404985d
24311a05q9-cmd <24311a05q9@cse.sreenidhi.edu.in>
2026-07-27 21:58:55 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Personal Portfolio Website
+
+## Features
+
+- Responsive Design
+- About Section
+- Projects Section
+- Contact Form
+- Smooth Scrolling
+- Mobile Friendly
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Developed For
+
+ApexPlanet Software Pvt. Ltd.
+Web Development Internship - Task 4
--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-const images = [
-"https://picsum.photos/id/1015/700/400",
-"https://picsum.photos/id/1016/700/400",
-"https://picsum.photos/id/1018/700/400",
-"https://picsum.photos/id/1025/700/400"
-];
-let index = 0;
-function showImage(){
-    document.getElementById("sliderImage").src = images[index];
+function scrollToProjects(){
+document.getElementById("projects").scrollIntoView({
+behavior:"smooth"
+});
 }
-function nextSlide(){
-    index++;
-    if(index >= images.length){
-        index = 0;
-    }
-    showImage();
-}
-function prevSlide(){
-    index--;
-    if(index < 0){
-        index = images.length-1;
-    }
-    showImage();
-}
-setInterval(nextSlide,3000);
-async function getJoke(){
-    try{
-        let response = await fetch("https://official-joke-api.appspot.com/random_joke");
-        let data = await response.json();
-        document.getElementById("joke").innerHTML =
-        "<strong>" + data.setup + "</strong><br><br>" + data.punchline;
-    }
-    catch(error){
-        document.getElementById("joke").innerHTML =
-        "Unable to fetch joke.";
 
-    }
-}
+document.getElementById("contactForm").addEventListener("submit",function(e){
+
+e.preventDefault();
+
+document.getElementById("msg").innerHTML=
+"Thank you! Your message has been received.";
+
+this.reset();
+
+});
